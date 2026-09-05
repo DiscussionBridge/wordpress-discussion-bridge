fd8c07d554e4a2a71bd5332a46a88c53a69e5b17
ph1958 <33202278+ph1958@users.noreply.github.com>
2026-09-04 19:33:37 -0700

    Align package with DiscussionBridge Alpha.19

--- blocks/from-discourse/index.asset.php
+++ blocks/from-discourse/index.asset.php
@@ -2,5 +2,5 @@
 
 return [
     'dependencies' => ['wp-blocks', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n'],
-    'version' => '0.1.0-alpha.17',
+    'version' => '0.2.0-alpha.19',
 ];
